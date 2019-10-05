15969c8b7e138ba00243cc55aa5f017d021eac52
Ingrid Amalie <iasolbjorg@gmail.com>
2019-10-05 15:24:19 +0200

    Update led_board with power_up and power_down

--- led_board.py
+++ led_board.py
@@ -18,12 +18,12 @@ class LedBoard:
     def __init__(self):
         """ initialize """
         self.pins = {
-            "led1": LED(13),
-            "led2": LED(19),
-            "led3": LED(26),
-            "led4": LED(16),
-            "led5": LED(20),
-            "led6": LED(21)}
+            1: LED(13),
+            2: LED(19),
+            3: LED(26),
+            4: LED(16),
+            5: LED(20),
+            6: LED(21)}
 
     def setup(self):
         """ set the proper mode """
@@ -42,13 +42,116 @@ class LedBoard:
     def flash_all_leds(self, sec):
         """ flash all LEDs on and off for
             'sec' seconds when password is wrong """
+        # time_flashed is the duration the LEDs have flashed
+        time_flashed = 0
+        # last_state remembers the last state of the LED (1 = high, 0 = low)
+        last_state = 0
+
+        while time_flashed < sec:
+            for key in self.pins:
+                if last_state == 0:
+                    gpio.output(self.pins[key], gpio.HIGH)
+                else:
+                    gpio.output(self.pins[key], gpio.LOW)
+
+            if key == 6:
+                last_state = (last_state + 1) % 2
+
+            sleep(0.2)
+            time_flashed += 0.2
 
     def twinkle_all_leds(self, sec):
         """ turn all LEDs on and off in sequence
             for 'sec' seconds when password is verified """
+        for key in self.pins:
+            gpio.output(self.pins[key], gpio.HIGH)
+            sleep(sec / 6)
+            gpio.output(self.pins[key], gpio.LOW)
 
     def power_up(self):
         """ light show on power up """
+        gpio.output(self.pins[1], gpio.HIGH)
+        gpio.output(self.pins[3], gpio.HIGH)
+        gpio.output(self.pins[5], gpio.HIGH)
+        sleep(0.5)
+
+        gpio.output(self.pins[1], gpio.LOW)
+        gpio.output(self.pins[3], gpio.LOW)
+        gpio.output(self.pins[5], gpio.LOW)
+        gpio.output(self.pins[2], gpio.HIGH)
+        gpio.output(self.pins[4], gpio.HIGH)
+        gpio.output(self.pins[6], gpio.HIGH)
+        sleep(0.5)
+
+        gpio.output(self.pins[2], gpio.LOW)
+        gpio.output(self.pins[4], gpio.LOW)
+        gpio.output(self.pins[6], gpio.LOW)
+
+        gpio.output(self.pins[1], gpio.HIGH)
+        sleep(0.1)
+
+        gpio.output(self.pins[1], gpio.LOW)
+        gpio.output(self.pins[2], gpio.HIGH)
+        sleep(0.1)
+
+        gpio.output(self.pins[2], gpio.LOW)
+        gpio.output(self.pins[3], gpio.HIGH)
+        sleep(0.1)
+
+        gpio.output(self.pins[3], gpio.LOW)
+        gpio.output(self.pins[4], gpio.HIGH)
+        sleep(0.1)
+
+        gpio.output(self.pins[4], gpio.LOW)
+        gpio.output(self.pins[5], gpio.HIGH)
+        sleep(0.1)
+
+        gpio.output(self.pins[5], gpio.LOW)
+        gpio.output(self.pins[6], gpio.HIGH)
+        sleep(0.1)
+
+        gpio.output(self.pins[6], gpio.LOW)
 
     def power_down(self):
         """ light show on power down """
+        gpio.output(self.pins[2], gpio.HIGH)
+        gpio.output(self.pins[4], gpio.HIGH)
+        gpio.output(self.pins[6], gpio.HIGH)
+        sleep(0.5)
+
+        gpio.output(self.pins[2], gpio.LOW)
+        gpio.output(self.pins[4], gpio.LOW)
+        gpio.output(self.pins[6], gpio.LOW)
+        gpio.output(self.pins[1], gpio.HIGH)
+        gpio.output(self.pins[3], gpio.HIGH)
+        gpio.output(self.pins[5], gpio.HIGH)
+        sleep(0.5)
+
+        gpio.output(self.pins[1], gpio.LOW)
+        gpio.output(self.pins[3], gpio.LOW)
+        gpio.output(self.pins[5], gpio.LOW)
+
+        gpio.output(self.pins[6], gpio.HIGH)
+        sleep(0.1)
+
+        gpio.output(self.pins[6], gpio.LOW)
+        gpio.output(self.pins[5], gpio.HIGH)
+        sleep(0.1)
+
+        gpio.output(self.pins[5], gpio.LOW)
+        gpio.output(self.pins[4], gpio.HIGH)
+        sleep(0.1)
+
+        gpio.output(self.pins[4], gpio.LOW)
+        gpio.output(self.pins[3], gpio.HIGH)
+        sleep(0.1)
+
+        gpio.output(self.pins[3], gpio.LOW)
+        gpio.output(self.pins[2], gpio.HIGH)
+        sleep(0.1)
+
+        gpio.output(self.pins[2], gpio.LOW)
+        gpio.output(self.pins[1], gpio.HIGH)
+        sleep(0.1)
+
+        gpio.output(self.pins[1], gpio.LOW)
